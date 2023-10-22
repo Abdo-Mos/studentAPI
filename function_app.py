ee87af1dcdc67866ae979b79388a2ab91f936066
Abdo-Mos <aabbddoo.sshh@gmail.com>
2023-10-22 15:20:50 -0400

    feat: MangoDB connection test

--- function_app.py
+++ function_app.py
@@ -6,24 +6,38 @@ import pymongo
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+#  initializing DB
+try: 
+    db_client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = db_client["main_db"]
+    db_collection = db["student"]
+except:
+    print("Error connecting to DB")
 
+func.HttpResponse.mimetype = 'application/json'
+func.HttpResponse.charset = 'utf-8'
+
+
+'''
+Ref code: return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+'''
 @app.route(route="studentAPI")
 def studentAPI(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    if req.method == 'GET':
+        stu = db_collection.find_one({'id': 1})
+        stu_JSON = {
+            'id':stu['id'],
+            'name': stu['name'],
+            'grade': stu['grade']
+        }
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            json.dumps(stu_JSON)
         )
+        return func.HttpResponse("This is a GET method, and it is working fine")
+    elif req.method == 'POST':
+        return func.HttpResponse("Now this is a POST and you should do it from POSTMAN!!!")
